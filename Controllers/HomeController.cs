9f87f49719a4e6b0b26ac0605dc3376e0e42e205
Vandana Garia <gariavandana@gmail.com>
2024-06-10 17:52:57 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using gitActionsDemo.Models;
+
+namespace gitActionsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(sampleText message)
+        {
+            ViewBag.Message = message.Message;
+            return View();
+        }
+
+    }
+}
---
+++ Models/sampleText.cs
@@ -0,0 +1,7 @@
+﻿namespace gitActionsDemo.Models
+{
+    public class sampleText
+    {
+        public string ? Message { get; set; }
+    }
+}
